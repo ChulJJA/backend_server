452692230add41e38d3a1ba7d19d92da76151176
CHUL SEUNG LEE <52488182+ChulJJA@users.noreply.github.com>
2025-09-01 17:53:03 -0400

    initial upload

---
+++ moim-backend/src/main/java/com/moim/moim_backend/MoimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moim.moim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoimBackendApplication.class, args);
+	}
+
+}
---
+++ moim-backend/src/test/java/com/moim/moim_backend/MoimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moim.moim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
